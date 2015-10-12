17a14de50321a9cbf51f4247069533068d043128
vinodkgutpta96 <vinodkgupta96@gmail.com>
2015-10-12 18:21:10 +0530

    added hw05

---
+++ hw05/gpioThru.c
@@ -0,0 +1,75 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+#include <signal.h>    // Defines signal-handling functions (i.e. trap Ctrl-C)
+#include "beaglebone_gpio.h"
+
+/****************************************************************
+ * Global variables
+ ****************************************************************/
+int keepgoing = 1;    // Set to 0 when ctrl-c is pressed
+
+/****************************************************************
+ * signal_handler
+ ****************************************************************/
+void signal_handler(int sig);
+// Callback called when SIGINT is sent to the process (Ctrl-C)
+void signal_handler(int sig)
+{
+    printf( "\nCtrl-C pressed, cleaning up and exiting...\n" );
+	keepgoing = 0;
+}
+
+int main(int argc, char *argv[]) {
+    volatile void *gpio_addr;
+    volatile unsigned int *gpio_oe_addr;
+    volatile unsigned int *gpio_datain;
+    volatile unsigned int *gpio_setdataout_addr;
+    volatile unsigned int *gpio_cleardataout_addr;
+    unsigned int reg;
+
+    // Set the signal callback for Ctrl-C
+    signal(SIGINT, signal_handler);
+
+    int fd = open("/dev/mem", O_RDWR);
+
+    printf("Mapping %X - %X (size: %X)\n", GPIO0_START_ADDR, GPIO0_END_ADDR, 
+                                           GPIO0_SIZE);
+
+    gpio_addr = mmap(0, GPIO0_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 
+                        GPIO0_START_ADDR);
+
+    gpio_oe_addr           = gpio_addr + GPIO_OE;
+    gpio_datain            = gpio_addr + GPIO_DATAIN;
+    gpio_setdataout_addr   = gpio_addr + GPIO_SETDATAOUT;
+    gpio_cleardataout_addr = gpio_addr + GPIO_CLEARDATAOUT;
+
+    if(gpio_addr == MAP_FAILED) {
+        printf("Unable to map GPIO\n");
+        exit(1);
+    }
+    printf("GPIO mapped to %p\n", gpio_addr);
+    printf("GPIO OE mapped to %p\n", gpio_oe_addr);
+    printf("GPIO SETDATAOUTADDR mapped to %p\n", gpio_setdataout_addr);
+    printf("GPIO CLEARDATAOUT mapped to %p\n", gpio_cleardataout_addr);
+
+    *gpio_oe_addr0 &= GPIO_03;
+	*gpio_oe_addr0 &= ~GPIO_07;    
+
+    printf("Button at GPIO_03 controls LED at GPIO_07\n");
+    while(keepgoing) {
+    	if(*gpio_datain & GPIO_03) {
+            *gpio_setdataout_addr= GPIO_07;
+    	} else {
+            *gpio_cleardataout_addr = GPIO_07;
+    	}
+        //usleep(1);
+    }
+
+    munmap((void *)gpio_addr, GPIO0_SIZE);
+    close(fd);
+    return 0;
+}
