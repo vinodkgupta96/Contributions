51f799f4741fcfb74a8fc630f344c671830ebcfc
vinodkgupta96 <vinodkgupta96@gmail.com>
2015-10-15 20:44:38 +0530

    added lab07

---
+++ lab07/gyro2_b13141.js
@@ -0,0 +1,42 @@
+//Program By vinod kumar
+
+
+var b = require('bonescript');
+var port = '/dev/i2c-2'; // Use bus 2
+var gy521 = 0x68;
+var ms = 2000; // Repeat time in ms
+b.i2cOpen(port, gy521);
+// i2cWriteBytes(port, command, bytes, [callback])
+b.i2cWriteBytes(port, 0x6b, [0]); // Wake up MPU6050
+setInterval(readData, ms)
+function readData() {
+// i2cReadBytes(port, command, length, [callback])
+b.i2cReadBytes(port, 0x3b, 14, displayData);
+}
+// 16-btt values are stored as high-byte, then low-byte
+// <<8 shifts lower bits to upper bit and | or's upper with lower
+// <<16 >>16 shift everything up 16 bits and then back, thus sign-extending
+// the value
+function displayData(x) {
+    var gy;
+    if (x.event === 'return') {
+        gy = {
+           /* accel: {
+                x: (((x.return[0] << 8 | x.return[1]) << 16) >> 16),
+                y: (((x.return[2] << 8 | x.return[3]) << 16) >> 16),
+                z: (((x.return[4] << 8 | x.return[5]) << 16) >> 16)
+            };*/
+            temp: {
+                x: (((x.return[6] << 8 | x.return[7]) << 16) >> 16)
+
+            }
+            /*
+            gyro: {
+                gx: ((((x.return[8] << 8) | x.return[9]) << 16) >> 16),
+                gyr: (((x.return[10] << 8 | x.return[11]) << 16) >> 16),
+                gz: (((x.return[12] << 8 | x.return[13]) << 16) >> 16)
+            }*/
+        }
+    };
+    console.log(gy);
+}
---
+++ lab07/readme.txt
@@ -0,0 +1,17 @@
+To: Mark A Yoder
+From: Vinod Kumar
+Subject: Lab 07 Submission
+Partner's Name : Shiva Verma Soni
+Name: Vinod Kumar
+Roll No.: B13141
+
+	
+
+
+
+In This Lab we find the acceleration of an object , the temperature around surroundings.
+	
+In this lab the Gyroscope is connected to Beaglebone given in lab digram. We  connected the 4 pins (vdd, GND ,XCL, XDA ) tobone. We used the analog input pins to read the data from the gyroscope. In the code "gyro_b13141.js" and "gyro2_b13141.js" data is sent in array with each element of data 8 bits each. 0 to 5  for accelorometer, (6 ,7) for  temperature sensor and (8 to 13) correspond to gyroscope readings. The gyroscope sends data in 8 bits each. we Sheifted bits  by eight unit and adding the corresponding elements together, we got the respective values of acceleration, temperature and gyroscope. 
+Conclusions:
+
+This lab gave some basic undestandings of the gyroscopic sensor, which is widely used in solving real world problem.This is will also increase the knowledge while doing peooject based on sensors.
